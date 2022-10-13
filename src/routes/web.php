7148fe821178f99dbb7839eaa40cc7cd7b61598f
Junyaaa <bb.junjun2@gmail.com>
2022-10-14 00:10:26 +0900

    register first-commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
-// Route::get('/', 'TaskController@index')->name('tasks.index');
+// Auth::routes();
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
+
+Route::get('/folders/{id}/tasks','App\Http\Controllers\TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
@@ -28,3 +31,7 @@ Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@c
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
